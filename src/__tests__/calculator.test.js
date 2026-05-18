0f1414bf61d4493a717b594cccedeb99fad13cee
clement lmd <clement.laumond@gmail.com>
2026-05-18 15:17:12 +0200

    test: suppression test version node

--- src/__tests__/calculator.test.js
+++ src/__tests__/calculator.test.js
@@ -1,29 +1,24 @@
 // src/__tests__/calculator.test.js
-const { add, subtract, multiply, divide } = require('../calculator');
+const { add, subtract, multiply, divide } = require("../calculator");
 
-describe('Calculator', () => {
-  test('add : 2 + 3 doit retourner 5', () => {
+describe("Calculator", () => {
+  test("add : 2 + 3 doit retourner 5", () => {
     expect(add(2, 3)).toBe(5);
   });
 
-  test('subtract : 10 - 4 doit retourner 6', () => {
+  test("subtract : 10 - 4 doit retourner 6", () => {
     expect(subtract(10, 4)).toBe(6);
   });
 
-  test('multiply : 3 * 4 doit retourner 12', () => {
+  test("multiply : 3 * 4 doit retourner 12", () => {
     expect(multiply(3, 4)).toBe(12);
   });
 
-  test('divide : 10 / 2 doit retourner 5', () => {
+  test("divide : 10 / 2 doit retourner 5", () => {
     expect(divide(10, 2)).toBe(5);
   });
 
-  test('divide : division par zéro lève une erreur', () => {
-    expect(() => divide(10, 0)).toThrow('Division par zéro impossible');
-  });
-
-  test('version Node.js', () => {
-    const major = parseInt(process.version.slice(1));
-    expect(major).toBeLessThan(20);
+  test("divide : division par zéro lève une erreur", () => {
+    expect(() => divide(10, 0)).toThrow("Division par zéro impossible");
   });
 });
